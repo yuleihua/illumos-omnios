c0bfedc6bb08973a6a0806ff6127a9b8e15b43bf
Dan McDonald <danmcd@omniti.com>
2014-09-18 15:35:50 +0000

    5182 user after free in blkdev (backport)

--- usr/src/uts/common/io/blkdev/blkdev.c
+++ usr/src/uts/common/io/blkdev/blkdev.c
@@ -1305,7 +1305,6 @@ bd_sched(bd_t *bd)
 		rv = xi->i_func(bd->d_private, &xi->i_public);
 		if (rv != 0) {
 			bp = xi->i_bp;
-			bd_xfer_free(xi);
 			bioerror(bp, rv);
 			biodone(bp);
 
@@ -1313,6 +1312,7 @@ bd_sched(bd_t *bd)
 			bd->d_qactive--;
 			kstat_runq_exit(bd->d_kiop);
 			list_remove(&bd->d_runq, xi);
+			bd_xfer_free(xi);
 		} else {
 			mutex_enter(&bd->d_iomutex);
 		}
