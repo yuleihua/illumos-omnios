d0d82afedca5b94adf6588b36b448f1c35eaf536
Andy Fiddaman <omnios@citrus-it.co.uk>
2018-05-08 09:58:18 +0000

    Time for r151027

--- usr/src/tools/env/omnios-illumos-gate.sh
+++ usr/src/tools/env/omnios-illumos-gate.sh
@@ -247,5 +247,5 @@ export PERL_PKGVERS=
 # To build IPS packages for the version you wish to update, ONNV_BUILDNUM must
 # match the version you're on.  Find this in /etc/release, and lose the 'r'.
 
-# Current bloody is r151025, so use '151025'.
-export ONNV_BUILDNUM=151025
+# Current bloody is r151027, so use '151027'.
+export ONNV_BUILDNUM=151027
