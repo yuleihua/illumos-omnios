70d99f4ac4ba123b0f0d6d0e198ea9ae5089cdd3
Ryan Zezeski <rpz@joyent.com>
2018-10-26 05:06:02 -0600

    BHYVE: OS-2340 vnics should support LSO OS-6778 MAC loopback traffic should avoid cksum work OS-6794 want LSO support in viona OS-7319 dangling ref in mac_sw_cksum() OS-7331 mac_sw_cksum() drops valid UDP traffic Reviewed by: Patrick Mooney <patrick.mooney@joyent.com> Reviewed by: Dan McDonald <danmcd@joyent.com> Approved by: Patrick Mooney <patrick.mooney@joyent.com>

--- usr/src/uts/common/io/dls/dls.c
+++ usr/src/uts/common/io/dls/dls.c
@@ -251,6 +251,7 @@ dls_promisc(dld_str_t *dsp, uint32_t new_flags)
 	uint32_t new_type = new_flags &
 	    ~(DLS_PROMISC_RX_ONLY | DLS_PROMISC_FIXUPS);
 	mac_client_promisc_type_t mptype = MAC_CLIENT_PROMISC_ALL;
+	uint16_t mac_flags = 0;
 
 	ASSERT(MAC_PERIM_HELD(dsp->ds_mh));
 	ASSERT(!(new_flags & ~(DLS_PROMISC_SAP | DLS_PROMISC_MULTI |
@@ -297,9 +298,7 @@ dls_promisc(dld_str_t *dsp, uint32_t new_flags)
 		 */
 		dsp->ds_promisc = new_flags;
 		err = mac_promisc_add(dsp->ds_mch, mptype,
-		    dls_rx_promisc, dsp, &dsp->ds_mph,
-		    (new_flags != DLS_PROMISC_SAP) ? 0 :
-		    MAC_PROMISC_FLAGS_NO_PHYS);
+		    dls_rx_promisc, dsp, &dsp->ds_mph, mac_flags);
 		if (err != 0) {
 			dsp->ds_promisc = old_flags;
 			return (err);
@@ -337,7 +336,7 @@ dls_promisc(dld_str_t *dsp, uint32_t new_flags)
 		/* Honors both after-remove and before-add semantics! */
 		dsp->ds_promisc = new_flags;
 		err = mac_promisc_add(dsp->ds_mch, mptype,
-		    dls_rx_promisc, dsp, &dsp->ds_mph, 0);
+		    dls_rx_promisc, dsp, &dsp->ds_mph, mac_flags);
 		if (err != 0)
 			dsp->ds_promisc = old_flags;
 	} else {
