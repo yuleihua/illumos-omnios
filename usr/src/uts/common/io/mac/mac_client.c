a578fcf713d96428670f6a0e1be8e07745475ea1
Patrick Mooney <pmooney@pfmooney.com>
2019-04-10 18:16:49 +0000

    BHYVE: OS-7727 want mac rx barrier function
    commit 39324c3c3e7
    
    Conflicts:
    	usr/src/uts/common/io/mac/mac.c
    	usr/src/uts/common/io/mac/mac_client.c
    	usr/src/uts/common/sys/mac_client.h
    	usr/src/uts/common/sys/mac_impl.h

--- usr/src/uts/common/io/mac/mac_client.c
+++ usr/src/uts/common/io/mac/mac_client.c
@@ -21,7 +21,7 @@
 
 /*
  * Copyright (c) 2008, 2010, Oracle and/or its affiliates. All rights reserved.
- * Copyright (c) 2014, Joyent, Inc.  All rights reserved.
+ * Copyright 2019 Joyent, Inc.
  * Copyright 2017 RackTop Systems.
  */
 
@@ -1624,6 +1624,32 @@ void
 mac_rx_clear(mac_client_handle_t mch)
 {
 	mac_rx_set(mch, mac_pkt_drop, NULL);
+}
+
+void
+mac_rx_barrier(mac_client_handle_t mch)
+{
+	mac_client_impl_t *mcip = (mac_client_impl_t *)mch;
+	mac_impl_t *mip = mcip->mci_mip;
+
+	i_mac_perim_enter(mip);
+
+	/* If a RX callback is set, quiesce and restart that datapath */
+	if (mcip->mci_rx_fn != mac_rx_def) {
+		mac_rx_client_quiesce(mch);
+		mac_rx_client_restart(mch);
+	}
+
+	/* If any promisc callbacks are registered, perform a barrier there */
+	if (mcip->mci_promisc_list != NULL || mip->mi_promisc_list != NULL) {
+		mac_cb_info_t *mcbi =  &mip->mi_promisc_cb_info;
+
+		mutex_enter(mcbi->mcbi_lockp);
+		mac_callback_barrier(mcbi);
+		mutex_exit(mcbi->mcbi_lockp);
+	}
+
+	i_mac_perim_exit(mip);
 }
 
 void
