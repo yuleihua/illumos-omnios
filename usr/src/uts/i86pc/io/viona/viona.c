5ae5e98af487252439a13eac2139e8014d83ee52
John Levon <john.levon@joyent.com>
2019-05-15 11:39:37 +0000

    OS-7768 illumos-joyent needs smatch fixes
    commit 68530537415
    
    Conflicts:
    	usr/src/cmd/bhyve/Makefile
    	usr/src/cmd/zfs/zfs_main.c
    	usr/src/cmd/zoneadmd/log.c
    	usr/src/cmd/zoneadmd/zoneadmd.c
    	usr/src/common/smbios/smb_info.c
    	usr/src/lib/libdemangle/common/rust.c
    	usr/src/lib/libresolv2_joy/Makefile.com
    	usr/src/lib/libvnd/Makefile.com
    	usr/src/lib/libzdoor/common/zdoor.c
    	usr/src/lib/libzdoor/common/ztree.c
    	usr/src/lib/libzonecfg/common/libzonecfg.c
    	usr/src/lib/varpd/libvarpd/common/libvarpd_prop.c
    	usr/src/uts/common/fs/dev/sdev_plugin.c
    	usr/src/uts/common/io/dr_sas/dr_sas.c
    	usr/src/uts/common/io/i40e/i40e_main.c
    	usr/src/uts/common/io/scsi/adapters/smrt/smrt_hba.c
    	usr/src/uts/intel/httpfilt/Makefile
    	usr/src/uts/intel/hv_shutdown/Makefile
    	usr/src/uts/intel/overlay/Makefile
    	usr/src/uts/intel/vmxnet/Makefile
    	usr/src/uts/intel/vnd/Makefile
    	usr/src/uts/sparc/httpfilt/Makefile

--- usr/src/uts/i86pc/io/viona/viona.c
+++ usr/src/uts/i86pc/io/viona/viona.c
@@ -1539,7 +1539,7 @@ viona_worker_rx(viona_vring_t *ring, viona_link_t *link)
 {
 	proc_t *p = ttoproc(curthread);
 
-	thread_vsetname(curthread, "viona_rx_%p", ring);
+	(void) thread_vsetname(curthread, "viona_rx_%p", ring);
 
 	ASSERT(MUTEX_HELD(&ring->vr_lock));
 	ASSERT3U(ring->vr_state, ==, VRS_RUN);
@@ -1564,7 +1564,7 @@ viona_worker_tx(viona_vring_t *ring, viona_link_t *link)
 {
 	proc_t *p = ttoproc(curthread);
 
-	thread_vsetname(curthread, "viona_tx_%p", ring);
+	(void) thread_vsetname(curthread, "viona_tx_%p", ring);
 
 	ASSERT(MUTEX_HELD(&ring->vr_lock));
 	ASSERT3U(ring->vr_state, ==, VRS_RUN);
